505a909ffcdd5afafcf935fe75312205026085c1
yuhanma <44038060+YuhanChelseaMa@users.noreply.github.com>
2019-11-24 03:03:28 -0500

    Update challenge1.js

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,10 +1,13 @@
 $("#subscribe").click(function(){
 	var x = $("#emailField");
 	if (this.checked){
+		console.log("Selected");
 		x.css("display","block");
 	}
 
 	else{
+		console.log("Not selected");
 		x.css("display","none");
 	}
 })
+
